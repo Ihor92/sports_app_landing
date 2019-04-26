6c64f02f42bbf2d8e635a4d94e3c309d05c34999
Ihor92 <salfetnikov92@gmail.com>
2019-04-26 15:31:47 +0300

    Fixed scroll #8

---
+++ src/index-js/main.js
@@ -0,0 +1,36 @@
+$("#carousel").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
+
+$("#carousel").slick({
+
+  infinite: false,
+
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
